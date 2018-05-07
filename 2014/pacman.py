94b98a965de5459a2dfcdcfac2f739573a780746
Selton Naoki Murakami <33372236+SeltonMurakami@users.noreply.github.com>
2018-05-07 02:33:13 -0300

    Tarefa: Pacman

---
+++ 2014/pacman.py
@@ -0,0 +1,19 @@
+n = int(input())
+a = ""
+up = 0
+temp = 0
+for i in range(n):
+	if i%2 == 0:
+		a += input()
+	else:
+		a += input()[::-1]
+for i in list(a):
+	if i == "o":
+		temp += 1
+	elif i == "A":
+		if temp > up:
+			up = temp
+		temp = 0
+if temp > up:
+	up = temp
+print(up)
