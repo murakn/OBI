6eac33e5a510ba8108b38fd35c62283f1074e4fb
Selton Naoki Murakami <33372236+SeltonMurakami@users.noreply.github.com>
2018-05-09 14:29:37 -0300

    Tarefa: Soma de

---
+++ 2012/casas.py
@@ -0,0 +1,9 @@
+n = int(input())
+c = []
+for i in range(n):
+	c.append(int(input()))
+k = int(input())
+for index, e in enumerate(c):
+	for i in c[index:]:
+		if e + i == k:
+			print(str(e)+" "+str(i))
